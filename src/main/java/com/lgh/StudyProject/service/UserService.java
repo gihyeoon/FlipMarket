1df7c7ba7912ddc0fb4baf3712102cd7e8d90760
Tublanx <rlgus03453@naver.com>
2025-04-13 12:02:03 +0900

    회원가입 시 유저, 관리자를 구분해주는 ROLE은 실제 추가할 때 앞에 "ROLE_"을 붙이면 안된다 -> 자동으로 생성해주기 때문에 붙이게 되면 ROLE_ROLE_USER가 되어서 오류가 발생

--- src/main/java/com/lgh/StudyProject/service/UserService.java
+++ src/main/java/com/lgh/StudyProject/service/UserService.java
@@ -1,12 +1,8 @@
 package com.lgh.StudyProject.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lgh.StudyProject.dto.UserDto;
 import com.lgh.StudyProject.entity.User;
 import com.lgh.StudyProject.repository.UserRepository;
 
@@ -17,7 +13,7 @@ public class UserService {
 	private UserRepository userRepository;
 
 	public void saveUser(String id, String pwd, String name, int age, String phoneNum) {
-		User user = new User(id, pwd, name, age, phoneNum, "ROLE_USER");
+		User user = new User(id, pwd, name, age, phoneNum, "USER");
 		userRepository.save(user);
 	}
 	
