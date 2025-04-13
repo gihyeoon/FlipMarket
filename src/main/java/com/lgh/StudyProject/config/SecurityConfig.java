113a1e2d9de1352d891863b6f6a043a521e4476a
Tublanx <rlgus03453@naver.com>
2025-04-13 12:01:08 +0900

    Spring Security로 로그인 시도 시 인증을 처리할 AuthenticationProvider를 찾을 수 없다는 오류가 발생. 이를 해결하기 위해 메서드 추가

--- src/main/java/com/lgh/StudyProject/config/SecurityConfig.java
+++ src/main/java/com/lgh/StudyProject/config/SecurityConfig.java
@@ -2,6 +2,10 @@ package com.lgh.StudyProject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,19 +19,48 @@ import com.lgh.StudyProject.service.CustomUserDetailsService;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final UserDetailsService userDetailsService;
+
 	private final CustomUserDetailsService customUserDetailsService;
-	
-	public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+
+	public SecurityConfig(CustomUserDetailsService customUserDetailsService, UserDetailsService userDetailsService) {
 		this.customUserDetailsService = customUserDetailsService;
+		this.userDetailsService = userDetailsService;
 	}
 	
+	@Bean
+	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+		AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManagerBuilder.class)
+				.userDetailsService(customUserDetailsService)
+				.passwordEncoder(passwordEncoder())
+				.and()
+				.build();
+		return authenticationManager;
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		
+		provider.setUserDetailsService(customUserDetailsService);
+		provider.setPasswordEncoder(passwordEncoder());
+		
+		return provider;
+	}
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(
 						auth -> auth.requestMatchers("/login", "/register").permitAll().anyRequest().authenticated())
-				.formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/dashboard", true).permitAll())
+				.formLogin(form -> form.loginPage("/login").loginProcessingUrl("/login")
+						.defaultSuccessUrl("/dashboard", true).permitAll().failureHandler((request, response, exception) -> {
+							System.out.println("로그인 실패 : " + exception.getMessage());
+							response.sendRedirect("/login?error");
+						}))
 				.logout(logout -> logout.logoutSuccessUrl("/login"));
+		
+		System.out.println("필터 체인 로그");
 
 		return http.build();
 	}
@@ -36,7 +69,7 @@ public class SecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	public UserDetailsService userDetailsService() {
 		return customUserDetailsService;
