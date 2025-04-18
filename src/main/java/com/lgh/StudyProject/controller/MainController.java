74253349a8b4c0bf8f25020d982a9a085593c408
Tublanx <rlgus03453@naver.com>
2025-04-18 15:29:36 +0900

    애플리케이션 실행 시 기본 url이 /main으로 갈 수 있도록 수정

--- src/main/java/com/lgh/StudyProject/controller/MainController.java
+++ src/main/java/com/lgh/StudyProject/controller/MainController.java
@@ -2,19 +2,20 @@ package com.lgh.StudyProject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller()
+@Controller
 public class MainController {
 
+	@GetMapping("/")
+	public String redirectMainForm() {
+		return "redirect:/main";
+	}
+	
 	@GetMapping("/main")
 	public String mainForm() {
+		
+		
 		return "main";
-	}
-
-	@PostMapping("/main")
-	public String main() {
-		return "";
 	}
 
 }
