7e7bf266a0493882431e57781052192108f083b5
Tublanx <rlgus03453@naver.com>
2025-04-22 14:33:11 +0900

    로그인한 유저의 아이디 값을 이용하여 PK인 num값을 DB에서 찾은 뒤, 이 값을 html로 넘기는 로직 추가

--- src/main/java/com/lgh/StudyProject/controller/MainController.java
+++ src/main/java/com/lgh/StudyProject/controller/MainController.java
@@ -1,10 +1,19 @@
 package com.lgh.StudyProject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.lgh.StudyProject.repository.UserRepository;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@GetMapping("/")
 	public String redirectMainForm() {
@@ -12,8 +21,15 @@ public class MainController {
 	}
 	
 	@GetMapping("/main")
-	public String mainForm() {
-		
+	public String mainForm(Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String id = authentication.getName();
+		if (!id.equals("anonymousUser")) {
+			Long num = userRepository.findById(id).get().getNum();
+			model.addAttribute("id", num);
+		} else {
+			model.addAttribute("id", "");
+		}
 		
 		return "main";
 	}
