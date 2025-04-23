883fe8b91544bb5ad92a19fb17b9a7d0a10cb4e3
Tublanx <rlgus03453@naver.com>
2025-04-23 22:52:39 +0900

    Repository 어노테이션 추가 및 아이디를 통해 존재하는 회원 정보가 있는지 확인하는 메서드 추가

--- src/main/java/com/lgh/StudyProject/repository/UserRepository.java
+++ src/main/java/com/lgh/StudyProject/repository/UserRepository.java
@@ -3,11 +3,15 @@ package com.lgh.StudyProject.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lgh.StudyProject.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findById(String id);
+	
+	boolean existsById(String id);
 	
 }
