0d566f0b9205dadd4900880106f50f6d256e859f
Tublanx <rlgus03453@naver.com>
2025-04-23 22:51:05 +0900

    회원가입 시 프론트로부터 로직을 처리하는 controller 추가

---
+++ src/main/java/com/lgh/StudyProject/controller/RegisterController.java
@@ -0,0 +1,49 @@
+package com.lgh.StudyProject.controller;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lgh.StudyProject.dto.UserDto;
+import com.lgh.StudyProject.service.UserService;
+
+@RestController
+@RequestMapping("/api")
+public class RegisterController {
+	
+	private final UserService userService;
+	
+	private final BCryptPasswordEncoder passwordEncoder;
+	
+	public RegisterController(UserService userService, BCryptPasswordEncoder passwordEncoder) {
+		this.userService = userService;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@GetMapping("/overlap/idRegister")
+	public Map<String, String> idRegister(@RequestParam(value = "id") String id) {
+		return Map.of("result", userService.checkDuplicateId(id) ? "1" : "0");
+	}
+	
+	@PostMapping("/register")
+	public ResponseEntity<String> register(@RequestBody UserDto userDto) {
+		System.out.println("여기 들어오긴합니까..?");
+		String encodedPwd = passwordEncoder.encode(userDto.getPwd());
+		userDto.setPwd(encodedPwd);
+		try {
+			userService.register(userDto);
+			return ResponseEntity.ok("success");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Register Fail: " + e.getMessage());
+		}
+	}
+	
+}
