31bc7a6fc3e3d74bc611eac2922eb0298942d516
Tublanx <rlgus03453@naver.com>
2025-04-23 22:53:31 +0900

    setter getter 추가0

--- src/main/java/com/lgh/StudyProject/dto/UserDto.java
+++ src/main/java/com/lgh/StudyProject/dto/UserDto.java
@@ -3,10 +3,15 @@ package com.lgh.StudyProject.dto;
 public class UserDto {
 
 	private String name;
+	
 	private String id;
+	
 	private String pwd;
+	
 	private int age;
+	
 	private String phoneNum;
+	
 	
 	public UserDto(String name, String id, String pwd, int age, String phoneNum) {
 		this.setName(name);
@@ -16,46 +21,57 @@ public class UserDto {
 		this.setPhoneNum(phoneNum);
 	}
 
+
 	public String getName() {
 		return name;
 	}
+
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
+
 	public String getId() {
 		return id;
 	}
+
 
 	public void setId(String id) {
 		this.id = id;
 	}
 
+
 	public String getPwd() {
 		return pwd;
 	}
+
 
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
 
+
 	public int getAge() {
 		return age;
 	}
+
 
 	public void setAge(int age) {
 		this.age = age;
 	}
 
+
 	public String getPhoneNum() {
 		return phoneNum;
 	}
 
+
 	public void setPhoneNum(String phoneNum) {
 		this.phoneNum = phoneNum;
 	}
+
 	
-	
+
 	
 }
