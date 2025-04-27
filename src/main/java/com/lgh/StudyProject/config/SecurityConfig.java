7a72304ad830735d16354730bac67add6919f62c
Tublanx <rlgus03453@naver.com>
2025-04-28 00:33:17 +0900

    이미지 파일 관련한 접근 권한 허용

--- src/main/java/com/lgh/StudyProject/config/SecurityConfig.java
+++ src/main/java/com/lgh/StudyProject/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(
-						auth -> auth.requestMatchers("/", "/login", "/register", "/main", "/api/**").permitAll().anyRequest().authenticated())
+						auth -> auth.requestMatchers("/", "/login", "/register", "/main", "/api/**", "/images/**", "/**.jpg", "/**.png").permitAll().anyRequest().authenticated())
 				.formLogin(form -> form.loginPage("/login").loginProcessingUrl("/login")
 						.defaultSuccessUrl("/main", true).permitAll()
 						.failureHandler((request, response, exception) -> {
