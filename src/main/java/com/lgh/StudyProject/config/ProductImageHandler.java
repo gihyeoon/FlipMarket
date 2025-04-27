5956811c6ee685d3059c5d530e771d462f1b974d
Tublanx <rlgus03453@naver.com>
2025-04-28 00:34:00 +0900

    이미지 파일을 저장하는 메서드의 반환 값 수정 => 원래는 절대경로의 파일 저장 경로를 반환하였으나 현재는 파일명을 반환하도록 수정

--- src/main/java/com/lgh/StudyProject/config/ProductImageHandler.java
+++ src/main/java/com/lgh/StudyProject/config/ProductImageHandler.java
@@ -9,15 +9,17 @@ public class ProductImageHandler {
 
 	public String save(MultipartFile image) throws IOException {
 		String fileName = getOriginName(image);
-		String filePath = "C:\\productImage\\" + fileName; // 이미지를 저장할 경로를 지정. 이 때 주의해야할 점은 절대 경로를 사용해야한다. (실제 서버를 운영하게 되면 그 서버의 이미지 파일을 저장하는 폴더의 절대 경로로 수정)
+		String filePath = "src/main/resources/static/images/" + fileName;
 		
+		// 나중에 실제 서버 운용해서 이미지 파일을 저장하게 될 때는 그 파일을 저장하는 로직을 추가해야함.
+
 		image.transferTo(new File(filePath));
-		
-		return filePath;
+
+		return fileName;
 	}
-	
+
 	private String getOriginName(MultipartFile image) {
 		return image.getOriginalFilename();
 	}
-	
+
 }
