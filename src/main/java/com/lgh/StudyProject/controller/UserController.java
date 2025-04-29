64d050002fa75829d590405b676b4fc2c5936ca3
Tublanx <rlgus03453@naver.com>
2025-04-29 21:47:32 +0900

    회원정보 재확인 로직 구현 완료

--- src/main/java/com/lgh/StudyProject/controller/UserController.java
+++ src/main/java/com/lgh/StudyProject/controller/UserController.java
@@ -1,20 +1,29 @@
 package com.lgh.StudyProject.controller;
 
+import java.util.Map;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lgh.StudyProject.entity.User;
 import com.lgh.StudyProject.repository.UserRepository;
 
 @Controller // Template을 사용할 경우에는 RestController는 사용하면 안된다.
 public class UserController {
-	
+
 	private final UserRepository userRepository;
 	
-	public UserController(UserRepository userRepository) {
+	private final BCryptPasswordEncoder passwordEncoder;
+
+	public UserController(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@GetMapping("/register")
@@ -26,17 +35,47 @@ public class UserController {
 	public String loginForm() {
 		return "login";
 	}
-	
+
 	@GetMapping("/mypage")
 	public String myPageForm(@RequestParam("num") Long num, Model model) {
 		User user = userRepository.findById(num).orElseThrow(() -> new RuntimeException("User not found"));
-		
+
+		model.addAttribute("num", num);
 		model.addAttribute("id", user.getId());
 		model.addAttribute("name", user.getName());
 		model.addAttribute("age", user.getAge());
 		model.addAttribute("phoneNum", user.getPhoneNum());
-		
+
 		return "mypage";
 	}
+
+	@GetMapping("/mypage/reConfirmUserInfo")
+	public String reConfirmUserInfoForm(@RequestParam("num") Long num, Model model) {
+		User user = userRepository.findById(num).orElseThrow(() -> new RuntimeException("User not found"));
+
+		model.addAttribute("num", num);
+		model.addAttribute("id", user.getId());
+		model.addAttribute("pwd", user.getPwd());
+
+		return "reConfirmUserInfo";
+	}
+
+	@ResponseBody
+	@PostMapping("/api/validateUser")
+	public int validateUser(@RequestBody Map<String, String> data) {
+		User user = userRepository.findById(data.get("id")).orElseThrow(() -> new RuntimeException("User not found"));
+		String loginUserPwd = user.getPwd();
+		
+		if (passwordEncoder.matches(data.get("enteredPwd"), loginUserPwd)) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 	
+	@GetMapping("/mypage/editProfile")
+	public String editProfileForm(@RequestParam("num") Long num) {
+		return "editProfile";
+	}
+
 }
