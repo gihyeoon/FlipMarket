20b7ca57051c62842f832c08d7ae2a30246bb8a4
Tublanx <rlgus03453@naver.com>
2025-05-06 22:05:31 +0900

    코드 리팩토링 완료 및 비밀번호 찾기 폼 접근 권한 추가

--- src/main/java/com/lgh/StudyProject/config/SecurityConfig.java
+++ src/main/java/com/lgh/StudyProject/config/SecurityConfig.java
@@ -7,12 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -31,14 +30,22 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-		AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManagerBuilder.class)
-				.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder()).and().build();
-		return authenticationManager;
+	AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
 	}
 
 	@Bean
-	public AuthenticationProvider authenticationProvider() {
+	BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		return customUserDetailsService;
+	}
+
+	@Bean
+	AuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 
 		provider.setUserDetailsService(customUserDetailsService);
@@ -48,13 +55,13 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(
-						auth -> auth.requestMatchers("/", "/login", "/register", "/main", "/api/**", "/images/**", "/**.jpg", "/**.png").permitAll().anyRequest().authenticated())
+						auth -> auth.requestMatchers("/", "/login", "/findPassword", "/register", "/main", "/api/**",
+								"/images/**", "/**.jpg", "/**.png").permitAll().anyRequest().authenticated())
 				.formLogin(form -> form.loginPage("/login").loginProcessingUrl("/login")
-						.defaultSuccessUrl("/main", true).permitAll()
-						.failureHandler((request, response, exception) -> {
+						.defaultSuccessUrl("/main", true).permitAll().failureHandler((request, response, exception) -> {
 							System.out.println("로그인 실패 : " + exception.getMessage());
 							response.sendRedirect("/login?error");
 						}))
@@ -69,28 +76,18 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		return customUserDetailsService;
-	}
-	
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
+	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		
+
 		corsConfiguration.setAllowedOrigins(List.of("http://localhost:8080"));
 		corsConfiguration.setAllowedMethods(List.of("GET", "POST"));
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedHeaders(List.of("*"));
 		corsConfiguration.setMaxAge(3600L);
-		
+
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", corsConfiguration);
-		
+
 		return source;
 	}
 
