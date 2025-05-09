08c0efc3acfcddca0c8c4863d8a4c23bf732b251
Tublanx <rlgus03453@naver.com>
2025-05-10 01:43:05 +0900

    아이디 -> 이메일 코드 수정 완료. 이메일 및 비밀번호 수정 시 Spring Security의 로그인 인증 정보 갱신하는 로직 추가

--- src/main/java/com/lgh/StudyProject/controller/user/UserApiController.java
+++ src/main/java/com/lgh/StudyProject/controller/user/UserApiController.java
@@ -4,6 +4,9 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lgh.StudyProject.dto.UserDto;
+import com.lgh.StudyProject.entity.User;
+import com.lgh.StudyProject.repository.UserRepository;
 import com.lgh.StudyProject.service.UserService;
 
 @CrossOrigin(origins = "http://localhost:8080")
@@ -21,18 +26,22 @@ import com.lgh.StudyProject.service.UserService;
 @RequestMapping("/api")
 public class UserApiController {
 
+	private final UserRepository userRepository;
+
 	private final UserService userService;
 
 	private final BCryptPasswordEncoder passwordEncoder;
 
-	public UserApiController(UserService userService, BCryptPasswordEncoder passwordEncoder) {
+	public UserApiController(UserRepository userRepository, UserService userService,
+			BCryptPasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
 		this.userService = userService;
 		this.passwordEncoder = passwordEncoder;
 	}
 
-	@GetMapping("/overlap/idRegister")
-	public Map<String, String> idRegister(@RequestParam(value = "id") String id) {
-		return Map.of("result", userService.checkDuplicateId(id) ? "1" : "0");
+	@GetMapping("/overlap/emailRegister")
+	public Map<String, String> emailRegister(@RequestParam(value = "email") String email) {
+		return Map.of("result", userService.checkDuplicateEmail(email) ? "1" : "0");
 	}
 
 	@PostMapping("/register")
@@ -47,26 +56,40 @@ public class UserApiController {
 		}
 	}
 
-	@PostMapping("/mypage/editProfile/updateId")
-	public ResponseEntity<String> updateId(@RequestBody Map<String, String> data) {
+	@PostMapping("/mypage/editProfile/updateEmail")
+	public ResponseEntity<String> updateEmail(@RequestBody Map<String, String> data) {
 		try {
-			int result = userService.updateId(data.get("id"), Long.parseLong(data.get("num")));
+			int result = userService.updateEmail(data.get("email"), Long.parseLong(data.get("num")));
 
 			if (result > 0) {
-				System.out.println("아이디 업데이트 성공");
+				System.out.println("이메일 업데이트 성공");
 			} else {
 				System.out.println("유저 정보 없음");
 			}
+
+			// 업데이트 이후 로그인 정보 갱신
+			UserDto userDto = userService.findByNum(Long.parseLong(data.get("num")));
+			User user = userRepository.findByEmail(userDto.getEmail())
+					.orElseThrow(() -> new RuntimeException("사용자 정보 없음"));
+
+			user.setEmail(userDto.getEmail());
+
+			Authentication auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(auth);
+
 			return ResponseEntity.ok("success");
 		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("updateId Fail: " + e.getMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("updateEmail Fail: " + e.getMessage());
 		}
 	}
 
 	@PostMapping("/mypage/editProfile/updatePhoneNum")
 	public ResponseEntity<String> updatePhoneNum(@RequestBody Map<String, String> data) {
+		Long num = Long.parseLong(data.get("num"));
+		String phoneNum = data.get("phoneNum");
+
 		try {
-			int result = userService.updatePhoneNum(data.get("phoneNum"), Long.parseLong(data.get("num")));
+			int result = userService.updatePhoneNum(phoneNum, num);
 
 			if (result > 0) {
 				System.out.println("휴대폰 번호 업데이트 성공");
@@ -75,7 +98,7 @@ public class UserApiController {
 			}
 			return ResponseEntity.ok("success");
 		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("updateId Fail: " + e.getMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("updatePhoneNum Fail: " + e.getMessage());
 		}
 	}
 
@@ -97,6 +120,13 @@ public class UserApiController {
 				System.out.println("비밀번호 변경할 유저 정보 없음");
 			}
 
+			User user = userRepository.findById(num).orElseThrow(() -> new RuntimeException("사용자 없음"));
+
+			user.setPwd(encodedNewPwd);
+
+			Authentication auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(auth);
+
 			return Map.of("result", "1");
 		} else {
 			return Map.of("result", "0");
@@ -106,7 +136,6 @@ public class UserApiController {
 	@PostMapping("/resetPassword/updatePwd")
 	public Map<String, String> resetPassword(@RequestBody Map<String, String> data) {
 		Long num = Long.parseLong(data.get("num"));
-
 		String newPwd = data.get("newPwd");
 		String originPwd = userService.selectUserPwdByNum(num);
 
@@ -123,14 +152,21 @@ public class UserApiController {
 				System.out.println("비밀번호 재설정 실패");
 			}
 
+			User user = userRepository.findById(num).orElseThrow(() -> new RuntimeException("사용자 없음"));
+
+			user.setPwd(encodedNewPwd);
+
+			Authentication auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(auth);
+
 			return Map.of("result", "1");
 		}
 	}
 
 	@PostMapping("/resetPassword")
 	public Map<String, String> countUserBefResetPassword(@RequestBody Map<String, String> data) {
-		String id = data.get("id");
-		int cnt = userService.countUserById(id);
+		String email = data.get("email");
+		int cnt = userService.countUserByEmail(email);
 
 		if (cnt == 0) {
 			return Map.of("result", "0");
