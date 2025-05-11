f3bf7e8f3a98242fcb3dd918bdccfe43cc4f28b0
Tublanx <rlgus03453@naver.com>
2025-05-11 21:40:31 +0900

    이미지 업로드 로직 수정 => 이미지 파일명이 겹칠 수도 있으므로 "현재 시간 값 + 이미지명" 으로 파일명 정의 후 저장

--- src/main/java/com/lgh/StudyProject/config/ProductImageHandler.java
+++ src/main/java/com/lgh/StudyProject/config/ProductImageHandler.java
@@ -8,14 +8,24 @@ import org.springframework.web.multipart.MultipartFile;
 public class ProductImageHandler {
 
 	public String save(MultipartFile image) throws IOException {
-		String fileName = getOriginName(image);
-		String filePath = "src/main/resources/static/images/" + fileName;
-		
+		String baseDir = System.getProperty("user.dir") + "/src/main/resources/static/images/";
+		String newFileName = System.currentTimeMillis() + "_" + getOriginName(image);
+		String filePath = baseDir + newFileName;
+
+		File uploadDir = new File(baseDir);
+		if (!uploadDir.exists()) {
+			boolean dirCreated = uploadDir.mkdirs(); // 디렉토리 없으면 생성
+			if (!dirCreated) {
+				throw new IOException("디렉토리 생성 실패 : " + uploadDir.getAbsolutePath());
+			}
+		}
+
 		// 나중에 실제 서버 운용해서 이미지 파일을 저장하게 될 때는 그 파일을 저장하는 로직을 추가해야함.
+		System.out.println(filePath);
 
 		image.transferTo(new File(filePath));
 
-		return fileName;
+		return newFileName;
 	}
 
 	private String getOriginName(MultipartFile image) {
