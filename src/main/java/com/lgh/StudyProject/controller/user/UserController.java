28ebfc5d0173533d5970cf0aa6ab8336700c3164
Tublanx <rlgus03453@naver.com>
2025-05-29 18:25:42 +0900

    마이페이지에서 로그인한 사용자가 가장 최근에 등록한 3개의 상품들을 보여주도록 하는 UI 추가. 각 상품 클릭 시에는 상품 수정 페이지로 이동됨

--- src/main/java/com/lgh/StudyProject/controller/user/UserController.java
+++ src/main/java/com/lgh/StudyProject/controller/user/UserController.java
@@ -1,72 +1,84 @@
 package com.lgh.StudyProject.controller.user;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.lgh.StudyProject.dto.ProductDto;
 import com.lgh.StudyProject.dto.UserDto;
+import com.lgh.StudyProject.service.ProductService;
 import com.lgh.StudyProject.service.UserService;
 
 @Controller
 public class UserController {
 
 	private final UserService userService;
+	
+	private final ProductService productService;
 
-	private final String baseUrl = "user/";
+	private static final String BASE_URL = "user/";
 
-	public UserController(UserService userService) {
+	public UserController(UserService userService, ProductService productService) {
 		this.userService = userService;
+		this.productService = productService;
 	}
 
 	@GetMapping("/register")
-	public String registerForm() {
+	public String showRegisterPage() {
 		return "register";
 	}
 
 	@GetMapping("/login")
-	public String loginForm() {
+	public String showLoginPage() {
 		return "login";
 	}
 
 	@GetMapping("/findPassword")
-	public String findPasswordForm() {
-		return baseUrl + "findPassword";
+	public String showFindPasswordPage() {
+		return BASE_URL + "findPassword";
 	}
 
 	@GetMapping("/mypage")
-	public String myPageForm(@RequestParam("num") Long userNum, Model model) {
+	public String showMypage(@RequestParam("num") Long userNum, Model model) {
 		UserDto userDto = userService.findByNum(userNum);
+		
+		// 사용자가 가장 최근에 등록한 3개의 상품들만 조회
+		List<ProductDto> recentProducts = productService.findByUserNum(userNum); 
+		
 		model.addAttribute("user", userDto);
-		return baseUrl + "mypage";
+		model.addAttribute("recentProducts", recentProducts);
+		return BASE_URL + "mypage";
 	}
 
 	@GetMapping("/mypage/reConfirmUserInfo")
-	public String reConfirmUserInfoForm(@RequestParam("num") Long userNum, Model model) {
+	public String showReConfirmUserInfoPage(@RequestParam("num") Long userNum, Model model) {
 		UserDto userDto = userService.findByNum(userNum);
 
 		model.addAttribute("num", userDto.getNum());
 		model.addAttribute("email", userDto.getEmail());
 		model.addAttribute("pwd", userDto.getPwd());
 
-		return baseUrl + "reConfirmUserInfo";
+		return BASE_URL + "reConfirmUserInfo";
 	}
 
 	@GetMapping("/mypage/editProfile")
-	public String editProfileForm(@RequestParam("num") Long userNum, Model model) {
+	public String showEditProfilePage(@RequestParam("num") Long userNum, Model model) {
 		UserDto userDto = userService.findByNum(userNum);
 		model.addAttribute("user", userDto);
-		return baseUrl + "editProfile";
+		return BASE_URL + "editProfile";
 	}
 
 	@GetMapping("/resetPassword")
-	public String resetPasswordForm(@RequestParam("email") String userEmail, Model model) {
+	public String showResetPasswordPage(@RequestParam("email") String userEmail, Model model) {
 		Long userNum = userService.findNumByEmail(userEmail);
 		UserDto userDto = userService.findByNum(userNum);
 
 		model.addAttribute("num", userDto.getNum());
 
-		return baseUrl + "resetPassword";
+		return BASE_URL + "resetPassword";
 	}
 
 }
--- src/main/java/com/lgh/StudyProject/service/UserService.java
+++ src/main/java/com/lgh/StudyProject/service/UserService.java
@@ -9,7 +9,6 @@ import com.lgh.StudyProject.repository.UserRepository;
 import jakarta.transaction.Transactional;
 
 @Service
-@Transactional
 public class UserService {
 
 	private final UserRepository userRepository;
@@ -18,6 +17,7 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 
+	// 마이페이지에서 로그인한 사용자 조회 시 사용
 	public UserDto findByNum(Long num) {
 		User user = userRepository.findById(num).orElseThrow(() -> new IllegalArgumentException("해당 유저는 존재하지 않습니다."));
 		
@@ -31,43 +31,51 @@ public class UserService {
 				.build();
 	}
 	
+	// 아이디(이메일)을 통해 사용자 번호 조회
 	public Long findNumByEmail(String email) {
 		return userRepository.findByEmail(email).get().getNum();
 	}
 	
+	// 회원가입
 	public void register(UserDto userDto) {
 		User user = new User(userDto.getEmail(), userDto.getPwd(), userDto.getName(), userDto.getAge(),
 				userDto.getPhoneNum(), "USER");
 		userRepository.save(user);
 	}
 
-	@Transactional
+	// 회원정보 수정 페이지에서의 이메일 수정
 	public int updateEmail(String newEmail, Long num) {
 		return userRepository.updateEmail(newEmail, num);
 	}
 
-	@Transactional
+	// 회원정보 수정 페이지에서의 연락처 수정
 	public int updatePhoneNum(String newPhoneNum, Long num) {
 		return userRepository.updatePhoneNum(newPhoneNum, num);
 	}
 
-	@Transactional
+	// 회원정보 수정 페이지에서의 비밀번호 수정
 	public int updatePwd(String newPwd, Long num) {
 		return userRepository.updatePwd(newPwd, num);
 	}
 
+	// 회원가입 및 회원정보 수정 시 이미 존재하는 이메일 정보가 있는지 확인
+	@Deprecated
 	public boolean checkDuplicateEmail(String email) {
 		return userRepository.existsByEmail(email);
 	}
 
+	// 사용자 번호를 통해 비밀번호 조회 (마이페이지에서 현재 비밀번호 비교를 하기 위함)
 	public String selectUserPwdByNum(Long num) {
 		return userRepository.selectUserPwdByNum(num);
 	}
 
+	// 회원가입 및 회원정보 수정 시 이미 존재하는 이메일 정보가 있는지 확인
 	public int countUserByEmail(String email) {
 		return userRepository.countUserByEmail(email);
 	}
 	
+	// 회원탈퇴
+	@Transactional
 	public void deleteByNum(Long userNum) {
 		userRepository.deleteById(userNum);
 	}
