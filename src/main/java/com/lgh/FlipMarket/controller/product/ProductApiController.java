96f1b2433b93ac8cd8fa29939c404f245cf8a6bb
Tublanx <rlgus03453@naver.com>
2025-06-03 19:50:51 +0900

    로그인 여부 파악하는 로직 수정 => OAuth2User일 경우도 추가

--- src/main/java/com/lgh/FlipMarket/controller/product/ProductApiController.java
+++ src/main/java/com/lgh/FlipMarket/controller/product/ProductApiController.java
@@ -3,8 +3,10 @@ package com.lgh.FlipMarket.controller.product;
 import java.io.IOException;
 import java.util.Map;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,7 +45,17 @@ public class ProductApiController {
 			@RequestParam("stock") int stock, @RequestParam("price") int price, @RequestParam("desc") String desc)
 			throws IOException {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		Long userNum = userService.findNumByEmail(authentication.getName());
+		String email = "";
+		Long userNum = 0L;
+
+		if (authentication.getPrincipal() instanceof OAuth2User) {
+			OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+			email = (String) oauth2User.getAttributes().get("email");
+		} else {
+			email = authentication.getName();
+		}
+		userNum = userService.findNumByEmail(email);
+
 		UserDto userDto = userService.findByNum(userNum);
 
 		// 로그인한 사용자가 상품명과 카테고리로 이미 등록되어있는 상품이 있는지 확인한다.
@@ -61,26 +73,36 @@ public class ProductApiController {
 	@PostMapping("/addCart")
 	public Map<String, String> addCart(@RequestBody Map<String, String> data) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String email = "";
+		Long productNum = Long.parseLong(data.get("productNum"));
 
-		if (authentication.getName().equals("anonymousUser")) {
-			return Map.of("result", "0");
-		} else {
-			Long productNum = Long.parseLong(data.get("productNum"));
-			Long userNum = userService.findNumByEmail(authentication.getName());
-
-			int count = cartService.countCartByUserNumAndProductNum(userNum, productNum);
-
-			if (count > 0) {
-				return Map.of("result", "2");
+		// 로그인이 되어있을 경우
+		if (authentication != null && authentication.isAuthenticated()
+				&& !(authentication instanceof AnonymousAuthenticationToken)) {
+			if (authentication.getPrincipal() instanceof OAuth2User) {
+				OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+				email = (String) oauth2User.getAttributes().get("email");
 			} else {
-				UserDto userDto = userService.findByNum(userNum);
-				ProductDto productDto = productService.findByNum(productNum);
-				int quantity = Integer.parseInt(data.get("quantity"));
-
-				cartService.addCart(userDto, productDto, quantity);
-
-				return Map.of("result", "1");
+				email = authentication.getName();
 			}
+		} else {
+			return Map.of("result", "0");
+		}
+
+		Long userNum = userService.findNumByEmail(email);
+
+		int count = cartService.countCartByUserNumAndProductNum(userNum, productNum);
+
+		if (count > 0) {
+			return Map.of("result", "2");
+		} else {
+			UserDto userDto = userService.findByNum(userNum);
+			ProductDto productDto = productService.findByNum(productNum);
+			int quantity = Integer.parseInt(data.get("quantity"));
+
+			cartService.addCart(userDto, productDto, quantity);
+
+			return Map.of("result", "1");
 		}
 	}
 
@@ -100,7 +122,7 @@ public class ProductApiController {
 
 		return Map.of("result", "0");
 	}
-	
+
 	@PostMapping("/deleteProduct")
 	public Map<String, String> deleteProduct(@RequestBody Map<String, String> data) {
 		Long productNum = Long.parseLong(data.get("num"));
