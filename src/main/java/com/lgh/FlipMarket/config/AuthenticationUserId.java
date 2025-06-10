6005784fb5c89156b89a0c1e33fde1ca41b9e111
Tublanx <rlgus03453@naver.com>
2025-06-10 13:09:20 +0900

    로그인된 사용자의 정보를 가져오는 클래스

---
+++ src/main/java/com/lgh/FlipMarket/config/AuthenticationUserId.java
@@ -0,0 +1,53 @@
+package com.lgh.FlipMarket.config;
+
+import java.util.Map;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import com.lgh.FlipMarket.service.UserService;
+
+@Configuration
+public class AuthenticationUserId {
+
+	private final UserService userService;
+
+	public AuthenticationUserId(UserService userService) {
+		this.userService = userService;
+	}
+
+	public Long getUserNum() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String email = "";
+		Long userNum = 0L;
+
+		if (authentication != null && authentication.isAuthenticated()
+				&& !(authentication instanceof AnonymousAuthenticationToken)) {
+			if (authentication.getPrincipal() instanceof OAuth2User) {
+				OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+
+				// 네이버 로그인
+				if ((String) oauth2User.getAttributes().get("email") == null) {
+					Map<String, Object> response = (Map<String, Object>) oauth2User.getAttributes().get("response");
+					email = (String) response.get("email");
+				}
+				// 구글 로그인
+				else {
+					email = (String) oauth2User.getAttributes().get("email");
+				}
+			} else {
+				email = authentication.getName();
+			}
+
+			userNum = userService.findNumByEmail(email);
+		} else {
+			userNum = 0L;
+		}
+
+		return userNum;
+	}
+
+}
