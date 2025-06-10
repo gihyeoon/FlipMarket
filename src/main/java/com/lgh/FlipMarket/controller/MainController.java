e8b9d60e0758a009ef7502c26eab2f272c349459
Tublanx <rlgus03453@naver.com>
2025-06-10 13:09:58 +0900

    로그인 정보를 가져오는 클래스 생성에 따른 코드 수정

--- src/main/java/com/lgh/FlipMarket/controller/MainController.java
+++ src/main/java/com/lgh/FlipMarket/controller/MainController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.lgh.FlipMarket.config.AuthenticationUserId;
 import com.lgh.FlipMarket.dto.ProductDto;
 import com.lgh.FlipMarket.service.LikeService;
 import com.lgh.FlipMarket.service.ProductService;
@@ -19,16 +20,17 @@ import com.lgh.FlipMarket.service.UserService;
 @Controller
 public class MainController {
 
-	private final UserService userService;
-
 	private final ProductService productService;
-	
+
 	private final LikeService likeService;
 
-	public MainController(UserService userService, ProductService productService, LikeService likeService) {
-		this.userService = userService;
+	private final AuthenticationUserId authenticationUserId;
+
+	public MainController(ProductService productService, LikeService likeService,
+			AuthenticationUserId authenticationUserId) {
 		this.productService = productService;
 		this.likeService = likeService;
+		this.authenticationUserId = authenticationUserId;
 	}
 
 	@GetMapping("/")
@@ -38,49 +40,21 @@ public class MainController {
 
 	@GetMapping("/main")
 	public String mainForm(@RequestParam(value = "keyword", required = false) String keyword, Model model) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String email = "";
-		Long userNum = 0L;
-
-		if (authentication.getPrincipal() instanceof OAuth2User) {
-			OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
-			// 네이버 로그인일 경우
-			if ((String) oauth2User.getAttributes().get("email") == null) {
-				Map<String, Object> response = (Map<String, Object>) oauth2User.getAttributes().get("response");
-				email = (String) response.get("email");
-			}
-			// 구글 로그인일 경우
-			else {
-				email = (String) oauth2User.getAttributes().get("email");
-			}
-			userNum = userService.findNumByEmail(email);
-			model.addAttribute("num", userNum);
-			System.out.println("소셜 로그인 이메일 : " + email);
-		} else {
-			email = authentication.getName();
-
-			if (!email.equals("anonymousUser")) {
-				userNum = userService.findNumByEmail(email);
-				model.addAttribute("num", userNum);
-			} else {
-				model.addAttribute("num", "");
-			}
-		}
+		Long userNum = authenticationUserId.getUserNum();
 
 		List<ProductDto> productList;
-
 		if (keyword != null && !keyword.isEmpty()) {
 			productList = productService.findByKeywordAndUserNum("%" + keyword + "%", userNum);
 		} else {
 			productList = productService.findByUserNumNot(userNum);
 		}
 
-		model.addAttribute("productList", productList);
-		
 		// 로그인한 사용자가 좋아요 누른 상품 번호들을 View로 넘김
 		List<Long> likeProductList = likeService.findByUserNum(userNum);
-		
+
+		model.addAttribute("num", userNum);
 		model.addAttribute("likeProductList", likeProductList);
+		model.addAttribute("productList", productList);
 
 		return "main";
 	}
